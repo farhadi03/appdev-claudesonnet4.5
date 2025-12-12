c7f717be2f5bf358deb7974bf2f745e9fd293648
farhadi03 <farhad.tahir@mycit.ie>
2025-12-12 08:40:34 +0000

    Initial commit: Spring Boot application setup

---
+++ src/main/java/com/example/claudesonnet/ClaudesonnetApplication.java
@@ -0,0 +1,13 @@
+package com.example.claudesonnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaudesonnetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClaudesonnetApplication.class, args);
+    }
+
+}
